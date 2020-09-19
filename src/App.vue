d70ffa812d86ec58679514815884b534092a2abc
Bushuev Zakhar <bushuevzi@mail.ru>
2020-09-19 22:08:11 +0300

    Todo list complete

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,63 @@
 <template>
-  <div id="app">
-    <h1>ToDo application</h1>
-    <br >
-    <TodoList v-bind:todos="todos"/>
-  </div>
+    <div id="app">
+        <h1>ToDo application</h1>
+        <AddTodo
+            v-on:add-todo="addTodoHandler"
+        />
+        <hr >
+        <TodoList
+                v-bind:todos="todos"
+                v-on:deleteTodo="deleteTodoHandler"
+        />
+    </div>
 </template>
 
 <script>
-  import TodoList from "@/components/TodoList";
+    import TodoList from "@/components/TodoList";
+    import AddTodo from "./components/AddTodo";
 
-export default {
-  name: 'App',
-  components: {
-    TodoList: TodoList
-  },
-  data(){
-    return {
-      todos: [
-        {id: 1, title: 'Видеокарта', completed: false},
-        {id: 2, title: 'Материнская плата', completed: false},
-        {id: 3, title: 'Процессор', completed: false},
-        {id: 4, title: 'ОЗУ', completed: false},
-        {id: 5, title: 'ПЗУ', completed: false},
-      ]
+    export default {
+        name: 'App',
+        components: {
+            TodoList: TodoList,
+            AddTodo: AddTodo
+        },
+        data() {
+            return {
+                todos: [
+                    // {id: 1, title: 'Видеокарта', completed: false},
+                    // {id: 2, title: 'Материнская плата', completed: false},
+                    // {id: 3, title: 'Процессор', completed: false},
+                    // {id: 4, title: 'ОЗУ', completed: false},
+                    // {id: 5, title: 'ПЗУ', completed: false},
+                ]
+            }
+        },
+        methods: {
+            deleteTodoHandler(id){
+                this.todos = this.todos.filter(t => t.id !== id)
+            },
+            addTodoHandler(item){
+                this.todos.push(item)
+            },
+            async loadTodos(count = 10) {
+                let response = await fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${count}`);
+                return await response.json()
+            }
+        },
+        async mounted() {
+            this.todos = await this.loadTodos(15)
+        }
     }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+    <form v-on:submit.prevent="onSubmit">
+        <input type="text" v-model="title">
+        <button type="submit">Create</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "AddTodo",
+        methods: {
+            onSubmit(){
+                console.log('Submit', this.title);
+                if (this.title.trim()){
+                    let newTodo = {
+                        id: Date.now(),
+                        title: this.title,
+                        completed: false
+                    };
+                    this.$emit('add-todo', newTodo);
+                    this.title = ''
+                }
+            }
+        },
+        data() {
+            return {
+                title: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input {
+        width: 400px;
+    }
+</style>
