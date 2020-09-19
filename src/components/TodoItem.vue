a6e293b015fad4e66cdd18af1dbf113f15f55337
Bushuev Zakhar <bushuevzi@mail.ru>
2020-09-20 00:50:21 +0300

    Todo list complete

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,63 @@
 <template>
     <li>
-        <input type="checkbox">
-        <strong>{{ item.id }}</strong>
+        <span v-bind:class="{done: todo.completed}">
+            <input type="checkbox" v-model="todo.completed">
+            <strong>{{index + 1}}</strong>
+            {{todo.title | capitalise}}
+        </span>
+        <button class="rm" v-on:click="$emit('removeTodo', todo.id)">&times;</button>
     </li>
 </template>
 
 <script>
     export default {
         name: "TodoItem",
-        props: ['item']
+        props: {
+            /**
+             * Элемент списка задач
+             */
+            todo: {
+                type: Object,
+                required: true
+            },
+
+            /**
+             * Порядковый номер элемента в массиве спизка задач todos
+             */
+            index: Number
+        },
+        filters: {
+            /**
+             * делает первую букву переданного String Заглавной
+             * @param string Значение, в котором необходимо сделать первую букву заглавную
+             * @returns Преобразованное строковое значение
+             */
+            capitalise(string){
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
+        }
     }
 </script>
 
 <style scoped>
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
 
+    }
+    .rm {
+        background-color: red;
+        color: #ffffff;
+        border-radius: 50%;
+        font-weight: bold;
+    }
+    input{
+        margin-right: 1rem;
+    }
+    .done {
+        text-decoration: line-through;
+    }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,22 +1,44 @@
 <template>
     <ul>
         <TodoItem
-                v-for="todo of todos"
+                v-for="(todo, index) of todos" v-bind:key="todo.id"
                 v-bind:todo="todo"
+                v-bind:index="index"
+                v-on:removeTodo="removeTodoHandler"
         />
     </ul>
 </template>
 
 <script>
-    import TodoItem from "@/components/TodoItem";
+    import TodoItem from "./TodoItem";
 
     export default {
         name: "TodoList",
-        components: {TodoItem},
-        props: ['todos']
+        components: {
+            TodoItem
+        },
+        props: {
+            /**
+             * Список задач
+             */
+            todos: Array
+        },
+        methods:{
+            /**
+             * Удаление элемента из списка задач todos
+             * @param id Индекс элемента в списке задач
+             */
+            removeTodoHandler(id){
+                this.$emit('deleteTodo', id)
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    ul{
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
 </style>
